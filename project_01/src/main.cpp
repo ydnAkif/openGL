a30d5236f8be28f51e2f03e695d20f425751fc90
ydnAkif <akifaydin52@gmail.com>
2021-11-07 13:10:34 +0300

    ok

--- project_01/src/main.cpp
+++ project_01/src/main.cpp
@@ -8,15 +8,19 @@ int main(int argc, char **argv)
 {
     GLFWwindow *window;
 
+    /* Initialize the library */
     if (!glfwInit())
     {
         return -1;
     }
 
+#ifdef __APPLE__
+    /* We need to explicitly ask for a 3.2 context on OS X */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
@@ -26,8 +30,10 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
