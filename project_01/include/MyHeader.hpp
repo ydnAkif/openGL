faf7e38945d2347d070bf6f7fcaebdc8a085f73c
ydnAkif <akifaydin52@gmail.com>
2021-11-07 12:51:17 +0300

    updated

---
+++ project_01/include/MyHeader.hpp
@@ -0,0 +1,6 @@
+#if !defined(MYHEADER_HPP)
+#define MYHEADER_HPP
+
+int topla(int, int);
+
+#endif // MYHEADER_HPP
---
+++ project_01/src/MyHeader.cpp
@@ -0,0 +1,6 @@
+#include "MyHeader.hpp"
+
+int topla(int a, int b)
+{
+    return a + b;
+}
